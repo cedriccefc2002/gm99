dce89c473bc3c83c38cea953197b7de5007ad78b
曾德雄 <cedriccefc2002@gmail.com>
2017-09-28 15:02:58 +0800

    Disable Hardware Acceleration

--- index.js
+++ index.js
@@ -24,14 +24,24 @@ switch (process.platform) {
 }
 electron_1.app.commandLine.appendSwitch('ppapi-flash-path', path.join(__dirname, 'plugins', pluginName));
 //app.commandLine.appendSwitch('ppapi-flash-version', '17.0.0.169')
+//Hardware accelerated
+// app.commandLine.appendSwitch('ignore-gpu-blacklist', 'true');
+// app.commandLine.appendSwitch('enable-gpu-rasterization', 'true');
+// app.commandLine.appendSwitch('enable-zero-copy', 'true');
+// app.commandLine.appendSwitch('disable-software-rasterizer', 'true');
+//Disable Hardware Acceleration
+electron_1.app.disableHardwareAcceleration();
 electron_1.app.on('ready', () => __awaiter(this, void 0, void 0, function* () {
     let win = new electron_1.BrowserWindow({
         width: 1024,
         height: 768,
         webPreferences: {
-            plugins: true
+            plugins: true,
+            defaultEncoding: "UTF-8"
         }
     });
+    //check gpu status
+    //win.loadURL(`chrome://gpu`);
     win.loadURL(`file://${__dirname}/index.html`);
     //win.webContents.openDevTools();
 }));
--- index.ts
+++ index.ts
@@ -17,14 +17,26 @@ switch (process.platform) {
 app.commandLine.appendSwitch('ppapi-flash-path', path.join(__dirname, 'plugins', pluginName));
 //app.commandLine.appendSwitch('ppapi-flash-version', '17.0.0.169')
 
+//Hardware accelerated
+// app.commandLine.appendSwitch('ignore-gpu-blacklist', 'true');
+// app.commandLine.appendSwitch('enable-gpu-rasterization', 'true');
+// app.commandLine.appendSwitch('enable-zero-copy', 'true');
+// app.commandLine.appendSwitch('disable-software-rasterizer', 'true');
+
+//Disable Hardware Acceleration
+app.disableHardwareAcceleration();
+
 app.on('ready', async () => {
     let win = new BrowserWindow({
         width: 1024,
         height: 768,
         webPreferences: {
-            plugins: true
+            plugins: true,
+            defaultEncoding: "UTF-8"
         }
     })
+    //check gpu status
+    //win.loadURL(`chrome://gpu`);
     win.loadURL(`file://${__dirname}/index.html`);
     //win.webContents.openDevTools();
 })
